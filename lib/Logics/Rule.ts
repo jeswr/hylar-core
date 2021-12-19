782223d6d54fc689e320ae6787b7778748889629
Jesse Wright <jesse.wright@anu.edu.au>
2021-12-20 00:06:35 +1100

    WIP

--- lib/Logics/Rule.ts
+++ lib/Logics/Rule.ts
@@ -1,81 +1,26 @@
+import type * as RDF from '@rdfjs/types';
+
+export interface Rule {
+  premise: RDF.Quad[];
+  conclusion: RDF.Quad[] | false;
+}
+
 /**
- * Created by mt on 21/12/2015.
+ * A lazy quad source.
  */
-
-import * as Utils from '../Utils';
-import Fact from './Fact';
-
-/**
- * Rule in the form subClassOf(a, b) ^ subClassOf(b, c) -> subClassOf(a, c)
- * i.e. conjunction of facts
- * @param slf set of (left side) conjunctive facts
- * @param ra the consequence facts
- * @constructor
- */
-export default class Rule {
-  causes: Fact[];
-
-  operatorCauses: Fact[];
-
-  consequences: Fact[];
-
-  constants: any[];
-
-  constructor(slf, srf) {
-    this.causes = [];
-    this.operatorCauses = [];
-    this.consequences = srf;
-    this.constants = [];
-
-    for (const s of slf) {
-      if (!s.operatorPredicate) {
-        this.causes.push(s);
-      } else {
-        this.operatorCauses.push(s);
-      }
-    }
-
-    for (let i = 0; i < this.causes.length; i++) {
-      this.constants = Utils.uniques(this.constants, slf[i].constants);
-    }
-    for (let i = 0; i < this.consequences.length; i++) {
-      this.constants = Utils.uniques(this.constants, srf[i].constants);
-    }
-    this.orderCausesByMostRestrictive();
-  }
-
+ export interface IQuadSource {
   /**
-   * Convenient method to stringify a rule.
-   * @returns {string}
+   * Returns a (possibly lazy) stream that processes all quads matching the pattern.
+   *
+   * The returned stream MUST expose the property 'metadata'.
+   * The implementor is reponsible for handling cases where 'metadata'
+   * is being called without the stream being in flow-mode.
+   *
+   * @param {RDF.Term} subject   The exact subject to match, variable is wildcard.
+   * @param {RDF.Term} predicate The exact predicate to match, variable is wildcard.
+   * @param {RDF.Term} object    The exact object to match, variable is wildcard.
+   * @param {RDF.Term} graph     The exact graph to match, variable is wildcard.
+   * @return {AsyncIterator<RDF.Quad>} The resulting quad stream.
    */
-  toString() {
-    let factConj = '';
-    for (const key in this.causes) {
-      factConj += ` ^ ${this.causes[key].toString().substring(1).replace(/,/g, '')}`;
-    }
-    for (const key in this.operatorCauses) {
-      factConj += ` ^ ${this.operatorCauses[key].toString().substring(1).replace(/,/g, '')}`;
-    }
-    return `${factConj.substr(3)} -> ${this.consequences.toString().substring(1).replace(/,/g, '')}`;
-  }
-
-  /**
-   * Orders rule causes (inplace) from the most to the least restrictive.
-   * The least a cause have variables, the most it is restrictive.
-   */
-  orderCausesByMostRestrictive() {
-    this.causes = this.causes.map((cause) => {
-      const { quad } = cause;
-      let constantOccurences = 0;
-      if (quad) {
-        if (quad.subject.termType === 'Variable') constantOccurences += 1;
-        if (quad.predicate.termType === 'Variable') constantOccurences += 1;
-        if (quad.object.termType === 'Variable') constantOccurences += 1;
-      }
-      return { cause, constantOccurences };
-    }).sort(
-      // eslint-disable-next-line no-nested-ternary
-      ({ constantOccurences: x }, { constantOccurences: y }) => ((x > y) ? -1 : ((x < y) ? 1 : 0)),
-    ).map(({ cause }) => cause);
-  }
+  match: (subject: RDF.Term, predicate: RDF.Term, object: RDF.Term, graph: RDF.Term) => AsyncIterator<RDF.Quad>;
 }
--- lib/Logics/Solver.ts
+++ lib/Logics/Solver.ts
@@ -3,17 +3,9 @@
  * Created by pc on 27/01/2016.
  */
 
-import type { Quad, Term } from 'rdf-js';
-import { quad } from '@rdfjs/data-model';
-import Fact from './Fact';
-import * as Logics from './Logics';
-import * as Utils from '../Utils';
-import Rule from './Rule';
-
-/**
-* Core solver used to evaluate rules against facts
-* using pattern matching mechanisms.
-* /
+import type { Quad, Term } from '@rdfjs/types';
+import { quad, blankNode } from '@rdfjs/data-model';
+import { Rule } from './Rule';
 
 /**
 * Evaluates a set of rules over a set of facts.
@@ -21,10 +13,8 @@ import Rule from './Rule';
 * @param facts
 * @returns Array of the evaluation.
 */
-export async function evaluateRuleSet(rules: Rule[], facts: Fact[] | IterableIterator<Fact>, doTagging = false): Promise<Fact[]> {
-  const evaluate = doTagging ? evaluateThroughRestrictionWithTagging : evaluateThroughRestriction;
-  const promises = rules.map((rule) => evaluate(rule, facts));
-
+export async function evaluateRuleSet(rules: Rule[], quads: Quad[]): Promise<Quad[]> {
+  const promises = rules.map((rule) => evaluateThroughRestriction(rule, quads));
   return [].concat(...await Promise.all(promises));
 }
 
@@ -35,120 +25,41 @@ export async function evaluateRuleSet(rules: Rule[], facts: Fact[] | IterableIte
 * @param facts
 * @returns {Array}
 */
-export async function evaluateThroughRestriction(rule: Rule, facts: Fact[] | IterableIterator<Fact>): Promise<Fact> {
-  const mappingList = getMappings(rule, facts); const consequences = [];
-
-  checkOperators(rule, mappingList);
-  for (const mapping of mappingList) {
-    if (mapping) {
-      // Replace mappings on all consequences
-      for (const r of rule.consequences) {
-        consequences.push(substituteFactVariables(mapping, r, []));
-      }
+export async function evaluateThroughRestriction(rule: Rule, quads: Quad[]): Promise<Quad[]> {
+  return getMappings(rule, quads).flatMap((mapping) => {
+    if (!rule.conclusion) {
+      throw new Error('Inconsistent database');
     }
-  }
-  return consequences;
+    return rule.conclusion.map((q) => substituteFactVariables(mapping, q));
+  })
 }
 
-/**
-* Evaluates a rule over a set of facts through
-* restriction of the rule's causes with tagging.
-* @param rule
-* @param kb
-* @returns {Array}
-*/
-export function evaluateThroughRestrictionWithTagging(rule: Rule, kb: Fact[] | IterableIterator<Fact>) {
-  const mappingList = getMappings(rule, kb);
-  const consequences: Fact[] = [];
+export function getMappings(rule: Rule, quads: Quad[]) {
+  let currentCauses = [{
+    cause: rule.premise[0],
+    mapping: {},
+  }]
 
-  checkOperators(rule, mappingList);
-
-  for (let i = 0; i < mappingList.length; i++) {
-    if (mappingList[i]) {
-      // Replace mappings on all consequences
-      const iterationConsequences = [];
-      for (let j = 0; j < rule.consequences.length; j++) {
-        const consequence = substituteFactVariables(mappingList[i], rule.consequences[j], Logics.buildCauses(mappingList[i].__facts__));
-        consequences.push(consequence);
-        iterationConsequences.push(consequence);
-      }
-      try {
-        Logics.addConsequences(mappingList[i].__facts__, iterationConsequences);
-      } catch (e) {
-        throw new Error('Error when trying to add consequences on the implicit fact.');
-      }
-    }
-  }
-  return consequences;
-}
-
-// eslint-disable-next-line consistent-return
-export function checkOperators(rule, mappingList) {
-  const causes = rule.operatorCauses;
-  let operationToEvaluate;
-  let substitutedFact;
-
-  if (rule.operatorCauses.length === 0) return mappingList;
-
-  for (let i = 0; i < mappingList.length; i++) {
-    for (let j = 0; j < causes.length; j++) {
-      // @ts-ignore
-      substitutedFact = substituteFactVariables(mappingList[i], causes[j]);
-      operationToEvaluate = Utils.getValueFromDatatype(substitutedFact.subject)
-        + substitutedFact.predicate
-        + Utils.getValueFromDatatype(substitutedFact.object);
-    }
-    // TODO: REMOVE EVAL
-    // eslint-disable-next-line no-eval
-    if (!eval(operationToEvaluate)) {
-      delete mappingList[i];
-      break;
-    }
-  }
-}
-
-export function getMappings(rule: Rule, facts: Fact[] | IterableIterator<Fact>) {
-  let mappingList = [rule.causes[0]]; // Init with first cause
-
-  for (let i = 0; i < rule.causes.length; i++) {
-    mappingList = substituteNextCauses(mappingList, rule.causes[i + 1], facts);
-  }
-  return mappingList;
-}
-
-/**
-* Updates the mapping of the current cause
-* given the next cause of a rule, over a
-* set of facts.
-* @param currentCauses
-* @param nextCause
-* @param facts
-* @returns {Array}
-*/
-export function substituteNextCauses(currentCauses: Fact[], nextCause: Fact, facts: Fact[] | IterableIterator<Fact>) {
-  const substitutedNextCauses = []; const mappings = [];
-
-  for (const currentCause of currentCauses) {
-    for (const fact of facts) {
-      // Update the mapping using pattern matching
-      const newMapping = factMatches(fact, currentCause, currentCause.mapping ?? { __facts__: [] });
-
-      // If the current fact matches the current cause ...
-      if (newMapping) {
-        // If there are other causes to be checked...
-        if (nextCause) {
-          // Substitute the next cause's variable with the new mapping
-          substitutedNextCause = substituteFactVariables(newMapping, nextCause, []);
-          substitutedNextCause.mapping = newMapping;
-          substitutedNextCauses.push(substitutedNextCause);
-        } else {
-          // Otherwise, add the new mapping to the global mapping array
-          mappings.push(newMapping);
+  for (const nextCause of rule.premise.slice(1)) {
+    const newCauses = []
+    for (const currentCause of currentCauses) {
+      // const facts = dataset.match(currentCause.cause.subject, currentCause.cause.predicate, currentCause.cause.object, currentCause.cause.graph);
+  
+      for (const q of quads) {
+        const mapping = factMatches(q, currentCause.cause, currentCause.mapping);
+        if (mapping) {
+          newCauses.push({
+            cause: substituteFactVariables(mapping, nextCause),
+            mapping,
+          });
         }
       }
     }
+
+    currentCauses = newCauses
   }
-  return nextCause ? substitutedNextCauses : mappings;
+
+  return currentCauses.map((c) => c.mapping);
 }
 
 /**
@@ -159,37 +70,34 @@ export function substituteNextCauses(currentCauses: Fact[], nextCause: Fact, fac
 * @param mapping
 * @returns {*}
 */
-export function factMatches(fact: Fact, { quad: term }: Fact, mapping) {
+export function factMatches(fact: Quad, term: Quad, mapping: { [key: string]: Term }) {
   const localMapping = {};
 
+  function factElemMatches(factElem: Term, causeElem: Term) {
+    if (causeElem.termType === 'Variable') {
+      if (mapping[causeElem.value] && !(mapping[causeElem.value].equals(factElem))) return false;
+      localMapping[causeElem.value] = factElem;
+    }
+    return factElem.equals(causeElem);
+  }
+
   // Checks and update localMapping if matches
-  if (!term || !fact.quad || !factElemMatches(fact.quad.predicate, term.predicate, mapping, localMapping)
-  || !factElemMatches(fact.quad.object, term.object, mapping, localMapping)
-  || !factElemMatches(fact.quad.subject, term.subject, mapping, localMapping)) return false;
+  if (!factElemMatches(fact.predicate, term.predicate)
+  || !factElemMatches(fact.object, term.object)
+  || !factElemMatches(fact.subject, term.subject)) return false;
 
   // If an already existing uri has been mapped...
   // Merges local and global mapping
   for (const mapKey in mapping) {
-    if (mapKey === '__facts__') {
-      localMapping[mapKey] = Utils.uniques(mapping[mapKey], [fact]);
-    } else {
-      for (const key in localMapping) {
-        if (mapping[mapKey] === localMapping[key] && mapKey !== key) return false;
-      }
-      localMapping[mapKey] = mapping[mapKey];
+    for (const key in localMapping) {
+      // This is horribly innefficient, allow lookup in rev direction
+      if (mapping[mapKey] === localMapping[key] && mapKey !== key) return false;
     }
+    localMapping[mapKey] = mapping[mapKey];
   }
 
   // The new mapping is updated
   return localMapping;
-}
-
-export function factElemMatches(factElem: Term, causeElem: Term, globalMapping, localMapping) {
-  if (causeElem.termType === 'Variable') {
-    if (globalMapping[causeElem.value] && !(globalMapping[causeElem.value].equals(factElem))) return false;
-    localMapping[causeElem.value] = factElem;
-  }
-  return factElem.equals(causeElem);
 }
 
 /**
@@ -198,28 +106,24 @@ export function factElemMatches(factElem: Term, causeElem: Term, globalMapping, 
 * @param mapping
 * @returns {*}
 */
-export function substitute(elem: Term, mapping) {
-  if (elem.termType === 'BlankNode') return Logics.skolemize(mapping.__facts__, elem);
-  if (elem.termType === 'Variable' && elem.value in mapping) return mapping[elem.value];
+export function substitute(elem: Term, mapping: { [key: string]: Term }): Term {
+  // TODO: See if this is necessary
+  if (elem.termType === 'BlankNode') return blankNode();
+  if (elem.termType === 'Variable') {
+    if (!(elem.value in mapping)) {
+      throw new Error('Variable not found in mapping');
+    }
+    return mapping[elem.value];
+  }
   return elem;
 }
 
-/**
-* Substitutes fact's variable members (sub, pred, obj)
-* given the mapping.
-* @param mapping
-* @param notYetSubstitutedFact
-* @param causedBy
-* @returns {*}
-*/
-export function substituteFactVariables(mapping, notYetSubstitutedFact: Fact, causedBy: Fact[][]) {
-  const { quad: term } = notYetSubstitutedFact;
-  if (mapping === {} || !term) return notYetSubstitutedFact;
-  return new Fact(
-    quad<Quad>(
-      substitute(term.subject, mapping),
-      substitute(term.predicate, mapping),
-      substitute(term.object, mapping),
-    ), !causedBy, causedBy,
+export function substituteFactVariables(mapping: { [key: string]: Term }, term: Quad) {
+  return quad(
+    // @ts-ignore
+    substitute(term.subject, mapping),
+    substitute(term.predicate, mapping),
+    substitute(term.object, mapping),
+    term.graph // TODO: Double check this
   );
 }
