26ac8aa3cc3add64660482b60d9dd8f22e3e49b8
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2022-08-08 20:01:19 +0200

    feat: export logics functionality

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,4 @@ export * from './Rules';
 export * from './ParsingInterface';
 export { default as Fact } from './Logics/Fact';
 export { default as Rule } from './Logics/Rule';
+export * as Logics from './Logics/Logics';
